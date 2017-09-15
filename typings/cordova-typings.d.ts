a6fd8307cbfd3f538cce1ef227b578f353fd1de3
netset <tallent16@netsetsoftware.com>
2017-09-15 13:02:12 +0530

    New changes

---
+++ typings/cordova-typings.d.ts
@@ -0,0 +1,15 @@
+
+/// <reference path="../.vscode/typings/cordova/cordova.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Device.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Splashscreen.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/FileTransfer.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/FileSystem.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Toast.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/NetworkInformation.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/InAppBrowser.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Camera.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/StatusBar.d.ts"/>
+/// <reference path="../.vscode/typings/cordova-ionic/plugins/keyboard.d.ts"/>
+/// <reference path="../.vscode/typings/ionic/ionic.d.ts"/>
+/// <reference path="../.vscode/typings/angularjs/angular.d.ts"/>
+/// <reference path="../.vscode/typings/jquery/jquery.d.ts"/>
